13eeead5bc7921ef5f87f82e64be20c30ab9a55e
TubaComic <manumel1504@gmail.com>
2021-11-30 10:57:17 +0100

    Michl isch lost

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -28,10 +28,20 @@ public class Map {
     }
 
     public void showMap(){
+        System.out.print(roomsS2.get(1));
+        System.out.println(roomsS2.get(2));
+        System.out.print(roomsS2.get(3));
+        System.out.println(roomsS2.get(4));
+
         System.out.print(roomsS1.get(1));
         System.out.println(roomsS1.get(2));
         System.out.print(roomsS1.get(3));
         System.out.println(roomsS1.get(4));
+
+        System.out.print(roomsS0.get(1));
+        System.out.println(roomsS0.get(2));
+        System.out.print(roomsS0.get(3));
+        System.out.println(roomsS0.get(4));
     }
 
 
