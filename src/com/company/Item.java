f264a340db7da1173faf2d7470f8cbf134fc822c
TubaComic <manumel1504@gmail.com>
2021-12-03 13:01:15 +0100

    trash

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Item {
+    private String name;
+
+    public Item(String itemName){
+        this.name = itemName;
+    }
+
+    public String getItemName(){
+        return name;
+    }
 }
