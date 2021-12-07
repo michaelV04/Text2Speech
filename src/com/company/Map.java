8ab9b041dc6c80d88449453dab7551553a40a426
TubaComic <manumel1504@gmail.com>
2021-12-07 10:34:06 +0100

    Map changes

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -13,7 +13,7 @@ public class Map {
     ArrayList<Room> roomsS2 = new ArrayList<>();
 
 
-    public Map(String mapname,int mapSize){
+    public Map(String mapname, int mapSize) {
         this.name = mapname;
         this.mapSize = mapSize;
         for (int i = 0; i < 64; i++) {
@@ -22,43 +22,63 @@ public class Map {
             roomsS2.add(null);
         }
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 
-    public void addRoom(Room x,int position,int stage){
+    public void addRoom(Room x, int position, int stage) {
         if (stage == 1) {
             roomsS1.add(position, x);
             x.setPos(position);
-        }else if(stage == 0){
+        } else if (stage == 0) {
             roomsS0.add(position, x);
             x.setPos(position);
-        }else if(stage == 2){
+        } else if (stage == 2) {
             roomsS2.add(position, x);
             x.setPos(position);
         }
     }
 
-    public void showMap(){
-        for (int i = 0; i < mapSize; i = i+2) {
-            System.out.print(roomsS2.get(i)+"\t");
-            System.out.println(roomsS2.get(i+1));
+    public void showMap() {
+        System.out.println("Stage 2:");
+        for (int i = 0; i < mapSize; i = i + 2) {
+            if (roomsS2.get(i) != null) {
+                System.out.print(i+roomsS2.get(i).getRoomName() + "\t");
+            }
+            if (roomsS2.get(i + 1) != null) {
+                System.out.println(i+1+roomsS2.get(i + 1).getRoomName());
+            }
 
+            System.out.println("\n");
         }
-        System.out.println("\n");
-        for (int i = 0; i < mapSize; i = i+2) {
-            System.out.print(roomsS1.get(i)+"\t");
-            System.out.println(roomsS1.get(i+1));
+        System.out.println("Stage 1:");
+        for (int i = 0; i < mapSize; i = i + 2) {
+            if (roomsS1.get(i) != null) {
+                System.out.print(i+roomsS1.get(i).getRoomName() + "\t");
+            }
+            if (roomsS1.get(i + 1) != null) {
+                System.out.println(i+1+roomsS1.get(i + 1).getRoomName());
+            }
+            System.out.println("\n");
+        }
 
-        }
-        System.out.println("\n");
-        for (int i = 0; i < mapSize; i = i+2) {
-            System.out.print(roomsS0.get(i)+"\t");
-            System.out.println(roomsS0.get(i+1));
 
+        System.out.println("Stage 0:");
+        for (int i = 0; i < mapSize; i = i + 2) {
+            if (roomsS0.get(i) != null) {
+                System.out.print(i+roomsS0.get(i).getRoomName() + "\t");
+            }
+            if (roomsS0.get(i + 1) != null) {
+                System.out.println(i+roomsS0.get(i + 1).getRoomName());
+            }
+            System.out.println("\n");
         }
-        System.out.println("\n\n\n\n\n");
     }
+
+
+
+
 
     public int checkIfExitTrue(Room x,Room y,Map z){
         double add = Math.sqrt(mapSize);
