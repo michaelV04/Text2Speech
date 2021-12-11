2c53f22e5a2e2f31369e35ace80e96101ac7db2a
michaelV04 <stvolmic@bx.fallmerayer.it>
2021-12-12 00:57:31 +0100

    v.1.4

--- src/com/company/Mob.java
+++ src/com/company/Mob.java
@@ -3,7 +3,7 @@ package com.company;
 public class Mob extends Entities {
     Room actualRoom = new Room("x","x",0,0,0,0,0,0);
     Map actualMap = new Map("x",4);
-//
+//TODO mobs can carry items that give them buffs and that they can drop
 
     public Mob(String name,int hp, int ad, int armor, int crit, int lvl, int speed,Room destinationRoom,Map currentMap) {
         super(name,hp, ad, armor, crit, lvl, speed,currentMap,destinationRoom);
